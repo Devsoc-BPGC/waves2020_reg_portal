2e4aef1b40231be5ff49b7bf3ad366067b3581d2
Mayank Mathur <Mynk-9@users.noreply.github.com>
2020-05-19 18:13:59 +0530

    Registration Page barebones.
    The page is created with all the components modular. Right now, it's all barebones. Currently under further development.

---
+++ client/src/components/page_header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './page_header.scss';
+
+export default class PageHeading extends React.Component {
+    render() {
+        return (
+            <div className="header">
+
+            </div>
+        );
+    }
+}
---
+++ client/src/components/page_heading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './page_heading.scss'
+
+export default class AdjHeading extends React.Component {
+    render() {
+        return (
+            <div className="adj-heading">
+                <span>REG<br />ISTER</span>
+            </div>
+        );
+    }
+}
