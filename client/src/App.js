da87fb6855ab5e02a21151f3fff982369dd668cc
Mayank Mathur <Mynk-9@users.noreply.github.com>
2020-05-20 12:38:05 +0530

    Changes acc to PR review.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-// import TestComponent from './components/testComponent';
 
 function App() {
   return <div>APP</div>;
