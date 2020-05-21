08e15291e321c58628a84169383144916c8153e0
Mayank Mathur <mynk-9@users.noreply.github.com>
2020-05-21 19:09:47 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-// import TestComponent from './components/testComponent';
 import RegistrationPage from './pages/registrationPage';
 
 function App() {
