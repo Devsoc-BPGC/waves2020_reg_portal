648cad07798033e655ec644144ab60a389f0f9fd
Mayank Mathur <mynk-9@users.noreply.github.com>
2020-05-22 04:22:18 +0530

    Minor fix in footer.

--- client/src/pages/registrationPage.js
+++ client/src/pages/registrationPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 // import PageHeader from '../components/commonpageheader/CommonPageHeader';
 import MainPageHeader from '../components/mainpageheader/MainPageHeader';
+import Footer from '../components/footer/Footer';
 
 export default () => {
   return (
     <div>
       <MainPageHeader />
       {/* <PageHeader heading1='REG' heading2='ISTER' /> */}
+      <Footer />
     </div>
   );
 };
