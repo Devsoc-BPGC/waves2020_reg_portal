7b7fff257da744adeb46fc18fb7cc459e510423d
Mayank Mathur <mynk-9@users.noreply.github.com>
2020-05-22 14:16:50 +0530

    Resolve conflicts with master.

--- client/src/pages/registrationPage.js
+++ client/src/pages/registrationPage.js
@@ -7,6 +7,8 @@ import Footer from '../components/footer/Footer';
 export default () => {
   return (
     <div>
+      {/* <PageHeader heading1='REG' heading2='ISTER' /> */}
+
       <MainPageHeader />
       <Footer />
     </div>
