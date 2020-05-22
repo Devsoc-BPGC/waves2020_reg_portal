d4be07862c66599e06d83229dd419aad54cfdd06
Mayank Mathur <mynk-9@users.noreply.github.com>
2020-05-22 14:09:12 +0530

    Animations and minor fixes.

--- client/src/pages/registrationPage.js
+++ client/src/pages/registrationPage.js
@@ -1,3 +1,4 @@
+// eslint: disable
 import React from 'react';
 // import PageHeader from '../components/commonpageheader/CommonPageHeader';
 import MainPageHeader from '../components/mainpageheader/MainPageHeader';
@@ -7,7 +8,6 @@ export default () => {
   return (
     <div>
       <MainPageHeader />
-      {/* <PageHeader heading1='REG' heading2='ISTER' /> */}
       <Footer />
     </div>
   );
